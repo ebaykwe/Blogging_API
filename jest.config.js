54b48db4e0129a65e29b651d404a3c43e979fbef
Emmanuel Ibekwe <153129922+ebaykwe@users.noreply.github.com>
2024-04-21 16:47:14 -0700

    jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,6 @@
 export default {
     verbose: true,
     testEnvironment: 'node',
-    // other configurations you might need
   };
-  
-// // jest.config.cjs
-// module.exports = {
-//     transform: {},
-//     testEnvironment: 'node',
-//     extensionsToTreatAsEsm: ['.js'],
-//     moduleNameMapper: {
-//       "^(\\.{1,2}/.*)\\.js$": "$1"
-//     }
-//   };
+ 
   
