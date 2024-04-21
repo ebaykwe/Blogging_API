6a76f0756aa920bb0e6622246c61013e58dfc972
Emmanuel Ibekwe <153129922+ebaykwe@users.noreply.github.com>
2024-04-21 16:40:50 -0700

    blogRoutes.js

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -5,15 +5,14 @@ import { protect, checkBlogOwner } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
 router.route('/')
-  .get(getBlogs)  // Public: Get all published blogs
-  .post(protect, createBlog);  // Protected: Create a new blog (default to draft)
-
+  .get(getBlogs)  
+  .post(protect, createBlog);  
 router.route('/:id')
-  .get(getBlogById)  // Public: Get a single blog by ID (must be published)
-  .put(protect, checkBlogOwner, updateBlog)  // Protected: Update a blog (if owner)
-  .delete(protect, checkBlogOwner, deleteBlog);  // Protected: Delete a blog (if owner)
+  .get(getBlogById)  
+  .put(protect, checkBlogOwner, updateBlog)  
+  .delete(protect, checkBlogOwner, deleteBlog); 
 
 router.route('/user/:userId')
-  .get(protect, getUserBlogs);  // Protected: Get blogs by user, filtered by state (draft or published)
+  .get(protect, getUserBlogs);  
 
 export default router;
